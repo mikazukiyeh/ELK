c0f833957956132366048746ea194fe32176e401
mikazukiusaki <mikazukiyeh@gmail.com>
2023-07-21 21:04:00 +0800

    新增樣式、HTML頁尾、以及新增導覽列的資料夾但還沒開始做

--- esg/src/app/app.module.ts
+++ esg/src/app/app.module.ts
@@ -6,17 +6,35 @@ import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
 import { MainitemComponent } from './mainitem/mainitem.component';
 import { IframeDialogComponent } from './iframe-dialog/iframe-dialog.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { NavComponent } from './nav/nav.component';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatButtonModule } from '@angular/material/button';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatIconModule } from '@angular/material/icon';
+import { MatListModule } from '@angular/material/list';
+import { MatSelectModule } from '@angular/material/select';
+import { OverlayModule } from '@angular/cdk/overlay';
 
 @NgModule({
   declarations: [
     AppComponent,
     HomeComponent,
     MainitemComponent,
-    IframeDialogComponent
+    IframeDialogComponent,
+    NavComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MatToolbarModule,
+    MatButtonModule,
+    MatSidenavModule,
+    MatIconModule,
+    MatListModule,
+    MatSelectModule,
+    OverlayModule
   ],
   providers: [],
   bootstrap: [AppComponent]
