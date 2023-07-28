2eb099758d1bf9af1a5d0613ca1549db51c12401
mikazukiusaki <mikazukiyeh@gmail.com>
2023-07-28 09:48:56 +0800

    刪除導覽列，因為暫時不知道要放什麼東西上去

--- esg/src/app/app.module.ts
+++ esg/src/app/app.module.ts
@@ -7,7 +7,6 @@ import { HomeComponent } from './home/home.component';
 import { MainitemComponent } from './mainitem/mainitem.component';
 import { IframeDialogComponent } from './iframe-dialog/iframe-dialog.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { NevComponent } from './nev/nev.component';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatButtonModule } from '@angular/material/button';
 import { MatSidenavModule } from '@angular/material/sidenav';
@@ -21,7 +20,6 @@ import { MatListModule } from '@angular/material/list';
     HomeComponent,
     MainitemComponent,
     IframeDialogComponent,
-    NevComponent,
   ],
   imports: [
     BrowserModule,
