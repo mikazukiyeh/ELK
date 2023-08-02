c464ea373c0ee8e4c7da6354d489994ec8be984c
mikazukiusaki <mikazukiyeh@gmail.com>
2023-08-02 14:20:51 +0800

    新增廢棄物跟工廠管制頁面的固定樣式，更改環保標章頁面的排版

--- esg/src/app/environmental/environmental.component.ts
+++ esg/src/app/environmental/environmental.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit,  HostListener } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 
 @Component({
@@ -6,60 +6,24 @@ import { Router } from '@angular/router';
   templateUrl: './environmental.component.html',
   styleUrls: ['./environmental.component.css']
 })
-export class EnvironmentalComponent {
+export class EnvironmentalComponent implements OnInit {
 
   currentIndex = 0;
   totalSlides = 2; // 總圖片數量
-  isMouseOver = false;
-  interval: any;
-  selectedOption = 'hotel'; 
+  selectedOption = 'hotel';
 
   constructor(private router: Router) { }
 
-  onMouseEnter() {
-    this.isMouseOver = true;
-  }
-
-  onMouseLeave() {
-    this.isMouseOver = false;
-  }
-
-  onClickSlide(event: MouseEvent) {
-    const slideWidth = (event.target as HTMLElement).offsetWidth;
-    const clickX = event.clientX;
-
-    if (this.isMouseOver) {
-      if (clickX < slideWidth / 2 && this.currentIndex > 0) {
-        this.currentIndex -= 1; // 向左滑動
-      } else if (clickX >= slideWidth / 2 && this.currentIndex < this.totalSlides - 1) {
-        this.currentIndex += 1; // 向右滑動
-      }
-    }
-  }
-
-  startAutoSlide() {
-    this.interval = setInterval(() => {
-      if (!this.isMouseOver && this.currentIndex < this.totalSlides - 1) {
-        this.currentIndex += 1;
-      }
-    }, 6000); // 每 6 秒切換一張圖片
-  }
-
-  stopAutoSlide() {
-    clearInterval(this.interval);
-  }
-
   ngOnInit() {
-    this.startAutoSlide();
-  }
-
-  ngOnDestroy() {
-    this.stopAutoSlide();
+    // 自動輪播切換
+    setInterval(() => {
+      this.currentIndex = (this.currentIndex + 1) % this.totalSlides;
+    }, 6000); // 每 6 秒切換一張圖片
   }
 
   // 回到首頁，使用 Angular 的路由導航
   navigateToHome(): void {
-    this.router.navigateByUrl('/home');
+    this.router.navigateByUrl('/home'); 
   }
 
   onOptionChange() {
