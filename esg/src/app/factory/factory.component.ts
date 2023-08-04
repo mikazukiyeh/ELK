67a376544cab5b18d48dbaf38b281b72822ad574
mikazukiusaki <mikazukiyeh@gmail.com>
2023-08-05 00:02:19 +0800

    更新iframe鑲嵌網址，增加安全性設置，但還是因為XSS危險無法鑲嵌

--- esg/src/app/factory/factory.component.ts
+++ esg/src/app/factory/factory.component.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { Router } from '@angular/router';
 import { cities, city } from '../city';
+import { DomSanitizer, SafeResourceUrl } from '@angular/platform-browser';
 
 @Component({
   selector: 'app-factory',
@@ -10,10 +11,17 @@ import { cities, city } from '../city';
 export class FactoryComponent {
   cities: city[] = cities;
   selectedCity: city | null = null;
+  sanitizedIframeUrl: SafeResourceUrl | null = null;
 
-  constructor(private router: Router) { }
+  constructor(private router: Router, private sanitizer: DomSanitizer) { }
 
   navigateToHome(): void {
     this.router.navigateByUrl('/home', { skipLocationChange: true });
+  }
+
+  updateSelectedCity(city: city): void {
+    this.selectedCity = city;
+    const encodedIframeUrl = encodeURIComponent(city.iframeUrl);
+    this.sanitizedIframeUrl = this.sanitizer.bypassSecurityTrustResourceUrl(encodedIframeUrl);
   }
 }
