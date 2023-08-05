f02643c109f501e72fc9b72e6f3924c122b3ae86
mikazukiusaki <mikazukiyeh@gmail.com>
2023-08-05 08:16:48 +0800

    將iframe網址設定安全設置後，現在終於可以顯現了。需要測試主機上的可以看到什麼

--- esg/src/app/factory/factory.component.ts
+++ esg/src/app/factory/factory.component.ts
@@ -1,27 +1,37 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 import { Router } from '@angular/router';
 import { cities, city } from '../city';
-import { DomSanitizer, SafeResourceUrl } from '@angular/platform-browser';
+import { DomSanitizer} from '@angular/platform-browser';
+
 
 @Component({
   selector: 'app-factory',
   templateUrl: './factory.component.html',
   styleUrls: ['./factory.component.css']
 })
-export class FactoryComponent {
-  cities: city[] = cities;
-  selectedCity: city | null = null;
-  sanitizedIframeUrl: SafeResourceUrl | null = null;
 
-  constructor(private router: Router, private sanitizer: DomSanitizer) { }
+
+export class FactoryComponent {
+  cities = cities;
+  selectedCity: city | null = null;
+  srcUrl: any;
+
+  @Input() city: string = '';
+  @Input() iframeUrl: string = '';
+ 
+
+  constructor(private router: Router, private sanitizer:DomSanitizer ) { }
 
   navigateToHome(): void {
     this.router.navigateByUrl('/home', { skipLocationChange: true });
   }
 
-  updateSelectedCity(city: city): void {
+  setSelectedCity(city: city | null): void {
     this.selectedCity = city;
-    const encodedIframeUrl = encodeURIComponent(city.iframeUrl);
-    this.sanitizedIframeUrl = this.sanitizer.bypassSecurityTrustResourceUrl(encodedIframeUrl);
+    this.srcUrl = city ? this.sanitizer.bypassSecurityTrustResourceUrl(city.iframeUrl) : null;
   }
+
+  // ngOnInit(){
+  //   this.srcUrl = this.sanitizer. bypassSecurityTrustResourceUrl(this.iframeUrl)
+  // }
 }
